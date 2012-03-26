394b22ffbd6b24a9dee5b1d2da6e6f5a882a6403
ganesh <ganesh@ganesh.(none)>
2012-03-26 23:19:38 +0530

    adding stem project"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stem::Application.config.secret_token = 'b40b31b2ae05f6cb18692e99bcc34f1cdfa3134b0d4899724da43ef0db6f389b0d2b6bf3ab5f5181ea95737b49ed6124ded2cd839b5b67d2d62315702d42ccfb'
