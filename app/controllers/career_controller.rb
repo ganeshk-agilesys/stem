67d1777ce4e6a6e3713e7bd2339cb9b4e56e7beb
Asit <asit@neevtech.com>
2012-04-11 18:46:38 +0530

    Menu links updated

--- app/controllers/career_controller.rb
+++ app/controllers/career_controller.rb
@@ -1,11 +1,11 @@
 class CareerController < ApplicationController
  def search
-    response = open URI.escape("http://127.0.0.1:3000/api/v1/industries.json")
+    response = open URI.escape("http://23.21.178.16/api/v1/industries.json")
     @industries = JSON.parse(response.read)
   end
    
   def search_result
-    @industries = JSON.parse(open(URI.escape("http://127.0.0.1:3000/api/v1/industries.json")).read).collect{|a| [a["name"],a["id"]]}
+    @industries = JSON.parse(open(URI.escape("http://23.21.178.16/api/v1/industries.json")).read).collect{|a| [a["name"],a["id"]]}
   end
 
 end
--- app/controllers/industry_controller.rb
+++ app/controllers/industry_controller.rb
@@ -1,13 +1,13 @@
 class IndustryController < ApplicationController
   def show
-    response = open URI.escape("http://127.0.0.1:3000/api/v1/industries/#{params[:id]}.json")
+    response = open URI.escape("http://23.21.178.16/api/v1/industries/#{params[:id]}.json")
     @industry = JSON.parse(response.read)
     @careers = @industry["related_careers"]
     @video = nil
     if @industry.has_key?("sub_industry_ids") && !@industry["sub_industry_ids"].nil?
       @industry_list = []
       @industry["sub_industry_ids"].each do |si|
-        sub_response = JSON.parse(open(URI.escape("http://127.0.0.1:3000/api/v1/industries/#{si}.json")).read)
+        sub_response = JSON.parse(open(URI.escape("http://23.21.178.16/api/v1/industries/#{si}.json")).read)
         @industry_list << {"sub_id" => sub_response["id"]}
         @industry_list << {"sub_name" => sub_response["name"]}
       end
