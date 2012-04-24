cddb3b44708fa731caf42e660cb2b81b830be113
Asit <asit@neevtech.com>
2012-04-24 17:56:58 +0530

    ui issues

--- app/helpers/industry_helper.rb
+++ app/helpers/industry_helper.rb
@@ -1,2 +1,8 @@
 module IndustryHelper
+  def industry_image(image_url,size_type='normal')
+    if image_url.present?
+       splitted_url = image_url.split(/(_normal-\w*)/) 
+       image_tag(splitted_url[0]+"_#{size_type}"+splitted_url[2])
+    end
+  end
 end
